47177b06244069a597530bb82597e6bda4fceeeb
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-28 21:49:06 +0100

    created variables i Js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // test connection file
 console.log('Js ok!');
+
+// create variables
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
