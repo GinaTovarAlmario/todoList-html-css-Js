54aa747eb020c2fe2fd90d53150981608ddf85b5
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-28 21:57:51 +0100

    defined function to add Task in Javascript

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ console.log('Js ok!');
 // create variables
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
+// define function 
+
+function addTask() {
+
+    // If the input box is empty, display an alert to the user
+    if (inputBox === '') {
+        alert('Please write something in your List!');
+    } else {
+        // If the input box is not empty, create a new list item (li)
+        let newTask = document.createElement("li");
+
+        // Set the content of the new list item to the value of the input box
+        newTask.innerHTML = inputBox.value;
+
+        // Append the new list item to the list container
+        listContainer.appendChild(newTask);
+
+        // Clear the input box after adding the task
+        inputBox.value = '';
+    }
+}
